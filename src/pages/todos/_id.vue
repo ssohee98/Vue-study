23d5feae9f007c4585b31a4877a81f8ebcb0218d
sohee jeong <jsohee98@naver.com>
2023-03-17 12:02:45 +0900

    todo/:id 경로 컴포넌트 _id.vue 만들고 id값 받아오기

---
+++ src/pages/todos/_id.vue
@@ -0,0 +1,19 @@
+<template>
+  <div> Todo Page </div>
+</template>
+
+<script>
+import {useRoute} from 'vue-router';
+export default {
+    setup() {
+        const route = useRoute();
+
+        //route에서 넘어오는 파라미터 id 확인
+        console.log(route.params.id);
+    }
+}
+</script>
+
+<style>
+
+</style>
