6b8a864b4341c569e9dd1e44f59443c679f17407
sohee jeong <jsohee98@naver.com>
2023-03-20 09:45:35 +0900

    onUnmouted() 사용하여 페이지 떠났을때 Toast 안나오도록

--- src/pages/todos/_id.vue
+++ src/pages/todos/_id.vue
@@ -40,7 +40,7 @@
 <script>
 import axios from 'axios';
 import {useRoute, useRouter} from 'vue-router';
-import {ref, computed, onBeforeMount, onMounted} from 'vue';
+import {ref, computed, onUnmounted} from 'vue';
 import _ from 'lodash';
 import Toast from '@/components/Toast.vue';
 export default {
@@ -49,12 +49,10 @@ export default {
        
     },
     setup(){
-        onBeforeMount(() => {
-            console.log(document.querySelector('#ssohee'));
-        });
-        
-        onMounted(() => {
-            console.log(document.querySelector('#ssohee'));
+        onUnmounted(() => {
+            console.log('unmounted');
+            clearTimeout(timeout.value);    
+            //unmount시(페이지 떠났을때) timeout값 초기화 (3초후에 toast 보여주지X)
         });
 
         const route = useRoute();
@@ -66,13 +64,15 @@ export default {
         const showToast = ref(false);
         const toastMessage = ref('');
         const toastAlertType = ref('');
+        const timeout = ref(null);
 
         //Toast.vue에 메시지 전달
         const triggerToast = (message, type='success') => {
             showToast.value = true;
             toastMessage.value = message;
             toastAlertType.value = type;
-            setTimeout(() => {
+            timeout.value = setTimeout(() => {  //setTimeout 값을 timeout에 넣기
+                console.log('hello');
                 toastMessage.value = '';
                 showToast.value = false;
                 toastAlertType.value = '';
